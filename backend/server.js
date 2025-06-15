1f3f6e593ab7db3d43d6d5d136aba7a5e3e6a303
Samarth Bhasin <78861159+bhasinsamarth@users.noreply.github.com>
2025-06-14 22:43:31 -0600

    fixed-the-backend-folder-not-working -on other-devices

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // backend/server.js
 import express from 'express';
 import cors from 'cors';
-import dotenv from '../dotenv';
+import dotenv from 'dotenv';
 import Mailjet from 'node-mailjet';
 
 dotenv.config();
